649d2f7ca3b29bac9edcec43fe33dd1aaa1777a3
Priyan Rai <54280820+inmert@users.noreply.github.com>
2025-06-17 19:06:45 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router"
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl">Welcome!</Text>
+      <Text className="text-5xl font-bold">Welcome!</Text>
     </View>
+
   );
 }
+
